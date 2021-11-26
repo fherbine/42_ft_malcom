2d90341d26df851f6e2ab18de9f1e90a161ac4bb
fherbine <felix.herbinet@yahoo.com>
2021-11-26 17:33:58 +0100

    error.c: message_exit & invalid_host_ip funcs

--- includes/ft_malcom.h
+++ includes/ft_malcom.h
@@ -1,3 +1,16 @@
 #ifndef FT_MALCOM_H
 # define FT_MALCOM_H
+
+/* includes */
+/** libft **/
+# include "../libft/libft.h"
+/** printf **/
+# include <stdio.h>
+
+/* prototypes */
+
+/** error.c **/
+void 	message_exit(char *msg, int status);
+void 	invalid_host_ip(char *host);
+
 #endif
--- sources/error.c
+++ sources/error.c
@@ -1,1 +1,13 @@
 #include "../includes/ft_malcom.h"
+
+void message_exit(char *msg, int status)
+{
+	dprintf((status == EXIT_FAILURE) ? STDERR : STDOUT, "ft_malcom: %s\n", msg);
+	exit(status);
+}
+
+void invalid_host_ip(char *host)
+{
+	dprintf(STDERR, "ft_malcom: unknown host or invalid IP address: (%s).", host);
+	exit(EXIT_FAILURE);
+}
